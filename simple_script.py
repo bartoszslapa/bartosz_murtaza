e870051f4be0455093e567d5d30a900d949acc2f
Bartosz <b.slapa@gmail.com>
2021-05-30 13:41:03 +0200

    Tidying a bit

--- simple_script.py
+++ simple_script.py
@@ -1,7 +1,8 @@
-
+# greeting Murtaza
 for a in range (1, 21):
-
     print ("Czesc Murtaza, for the", a, "time!")
+
+
 #finding the largest number
 largest_number = [1 , 2, 6 , 20]
 print (max.largest_number)
