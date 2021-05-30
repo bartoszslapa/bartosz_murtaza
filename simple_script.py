73738f872cca1504470b2a8762871970e182c4d5
Murtaza <murtazajits@gmail.com>
2021-05-30 13:39:50 +0200

    error in line 7

--- simple_script.py
+++ simple_script.py
@@ -1,10 +1,12 @@
 
 for a in range (1, 21):
-    
+
     print ("Czesc Murtaza, for the", a, "time!")
 #finding the largest number
 largest_number = [1 , 2, 6 , 20]
 print (max.largest_number)
+
+#there is an error in line 7
 
 
     
